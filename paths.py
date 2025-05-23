87c91ed9bdb18ebb0dc70b41d4be6e434573d7ba
Karl <luigiloza@gmail.com>
2025-05-23 13:47:40 +0800

    Add optimization results directory

--- paths.py
+++ paths.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import sys
 
+
 # set root directory
 ROOT_DIR = Path(__file__).resolve().parent
 # Add the root directory to the system path
@@ -20,6 +21,8 @@ IMPUTED_DATASETS_DIR = os.path.join(MICE_BASE_DIR, "Imputed Datasets")
 
 MODEL_METRICS_DIR = os.path.join(RESULTS_DIR, "Model Metrics")
 
+OPTIMIZATION_RESULTS_DIR = os.path.join(RESULTS_DIR, "Optimization Results")
+
 ### Files ###
 RAW_CSV_FILE = os.path.join(DATA_DIR, "start_data.csv")
 PROCESSED_CSV_FILE = os.path.join(DATA_DIR, "preprocessed_data.csv")
@@ -36,7 +39,8 @@ def make_directories():
               MICE_BASE_DIR, 
               MICE_IMPUTED_METRICS_DIR, 
               IMPUTED_DATASETS_DIR,
-              MODEL_METRICS_DIR]:
+              MODEL_METRICS_DIR,
+              OPTIMIZATION_RESULTS_DIR]:
         os.makedirs(d, exist_ok=True)
 
 
