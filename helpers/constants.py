295f95facdf3aa4de96b8c366f894be3c9715915
Karl <luigiloza@gmail.com>
2025-07-07 18:26:21 +0800

    Update constants.py
    add raw column names

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,6 +1,29 @@
+RAW_COLUMNS = [
+    "coating material",
+    "dopant element", 
+    "element composition",
+    "molar ratio",
+    "crystal space group",
+    "primary particle size",
+    "secondary particle size",
+    "precursor type",
+    "precursor preparation method", 
+    "precursor preparation conditions",
+    "annealing temperature",
+    "annealing time", 
+    "single crystal or polycrystalline",
+    "voltage range",
+    "electrolyte system	capacity",
+    "cycles", 
+    "capacity retention",
+    "first Coulombic efficiency",
+    "anode material"
+]
 
 TARGETS = [
     "capacity(mAh/g)", 
     "capacity_retention(%)", 
     "first_coulombic_efficiency(%)"
 ]
+
+
