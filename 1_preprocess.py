275917571e53ec7ffb5c3d3ebed5b0d7e0bacd57
Karl <karl_loza@sjtu.edu.cn>
2025-07-09 12:52:18 +0800

    Ready to run pipeline using a new approach

--- 1_preprocess.py
+++ 1_preprocess.py
@@ -1,87 +1,21 @@
-from helpers.function_map import function_map, parse_special_case
-from paths import RAW_DATA_DIR, RAW_CSV_FILE, PROCESSED_CSV_FILE, make_directories
-import polars as pl
-import os
+from helpers.function_map import TRANSFORMATION_RECIPE
+from paths import CLEANED_CSV_FILE, PROCESSED_CSV_FILE
+from ml_tools.ETL_engineering import DataProcessor
+from ml_tools.utilities import load_dataframe, save_dataframe
 
 
-def concatenate_raw_data() -> None:
+def process_data() -> None:
     """
-    Concatenates .csv files in the raw data directory and makes a new single .csv
+    Transform data to numerical values
     """
-    dfs = []
-    filenames = []
-    # Loop through all files in the raw data directory
-    for file in os.listdir(RAW_DATA_DIR):
-        if file.endswith(".csv"):
-            # Construct the full file path
-            df_path = os.path.join(RAW_DATA_DIR, file)
-            # Read the CSV file into a DataFrame, ignoring data types
-            df = pl.read_csv(df_path, infer_schema=False)
-            # Append the DataFrame to the list
-            dfs.append(df)
-            filenames.append(file)
+    data_processor = DataProcessor(TRANSFORMATION_RECIPE)
     
-    # Concatenate all DataFrames in the list into a single DataFrame
-    if dfs:
-        if len(dfs) > 1:
-            # Check if column names and order match
-            for i in range(1, len(dfs)):
-                if dfs[i].columns != dfs[0].columns:
-                    raise ValueError(f"Column names or order do not match in files:\n\t{filenames[0]}\n\t{filenames[i]}.")
-            concatenated_df = pl.concat(dfs, how="vertical")
-            # Write the concatenated DataFrame to a new CSV file
-            concatenated_df.write_csv(RAW_CSV_FILE)
-        else:
-            # If only one DataFrame, write it to the CSV file
-            dfs[0].write_csv(RAW_CSV_FILE)
-        print(f"Concatenated {len(dfs)} CSV files of raw data.")
-    else:
-        raise ValueError(f"No CSV files found in '{RAW_DATA_DIR}'")
+    df_cleaned = load_dataframe(df_path=CLEANED_CSV_FILE, kind="polars", all_strings=True)
     
+    df_processed = data_processor.transform(df=df_cleaned) # type: ignore
     
-def preprocess_data() -> None:
-    """
-    Preprocesses the data by applying functions from function_map
-    """
-    # If the data CSV file does not exist, create concatenate_raw_data 
-    if not os.path.exists(RAW_CSV_FILE):
-        concatenate_raw_data()
-    
-    # Read the data CSV file into a DataFrame
-    df = pl.read_csv(RAW_CSV_FILE, infer_schema=False)
-    
-    # Check that column names match to function map
-    for feature_name in df.columns:
-        if feature_name not in function_map.keys():
-            raise ValueError(f"Feature '{feature_name}' not found in function map.")
-    
-    # Apply the functions to the columns
-    results = []
-    for feature_name, function in function_map.items():
-        if function is not None:
-            output = function(df[feature_name])
-            # Check if the output is a Series or DataFrame
-            if isinstance(output, pl.Series):
-                results.append(output)
-            elif isinstance(output, pl.DataFrame):
-                results.extend(output.get_columns())
-            else:
-                raise ValueError(f"Function for '{feature_name}' did not return a Series or DataFrame.")
-    
-    # Special case
-    results.extend(parse_special_case(df).get_columns())
-    
-    if results:
-        # Make a new DataFrame from the results
-        results_df = pl.DataFrame(results)
-        # save the results to a new CSV file
-        results_df.write_csv(PROCESSED_CSV_FILE)
-        print(f"Preprocessed data saved to '{PROCESSED_CSV_FILE}'")
-    else:
-        raise ValueError("No results. Check the function map and data.")
+    save_dataframe(df=df_processed, save_dir=PROCESSED_CSV_FILE.parent, filename=PROCESSED_CSV_FILE.name)
 
 
 if __name__ == "__main__":
-    make_directories()
-    preprocess_data()
-    
+    process_data()
--- 6_optimization.py
+++ 6_optimization.py
@@ -1,26 +1,25 @@
-from ml_tools.PSO_optimization import run_pso, multiple_objective_functions_from_dir
+from ml_tools.PSO_optimization import run_pso, multiple_objective_functions_from_dir, plot_optimal_feature_distributions
 from ml_tools.utilities import deserialize_object
-from paths import OPTIMIZATION_RESULTS_DIR, OPTIMIZATION_MODELS_DIR, DATA_DIR
-import os
+from paths import OPTIMIZATION_RESULTS_DIR, OPTIMIZATION_MODELS_DIR, SERIALIZED_CONTINUOUS_FILE, SERIALIZED_BINARY_FILE
 
 
 def main():
     # load value range for continuous features 
-    cont_features_value_range: dict[str, tuple] = deserialize_object(os.path.join(DATA_DIR, "CONT_FEATURES_VALUE_RANGE_dict.joblib")) # type: ignore
+    cont_features_value_range: dict[str, tuple[float,float]] = deserialize_object(SERIALIZED_CONTINUOUS_FILE) # type: ignore
 
     # set boundaries
     lower_boundaries = [value[0] for key, value in cont_features_value_range.items()]
     upper_boundaries = [value[1] for key, value in cont_features_value_range.items()]
 
     # Set number of binary columns
-    binary_columns: list = deserialize_object(os.path.join(DATA_DIR, "BINARY_COLUMNS_list.joblib")) # type: ignore
+    binary_columns: list[str] = deserialize_object(SERIALIZED_BINARY_FILE) # type: ignore
     number_binary_columns = len(binary_columns)
 
     # Set objective functions
-    objective_functions, _objective_functions_names = multiple_objective_functions_from_dir(directory=OPTIMIZATION_MODELS_DIR, 
-                                                                                            add_noise=True, 
-                                                                                            task="maximization",
-                                                                                            binary_features=number_binary_columns)
+    objective_functions, _filenames = multiple_objective_functions_from_dir(directory=OPTIMIZATION_MODELS_DIR, 
+                                                                            add_noise=True, 
+                                                                            task="maximization",
+                                                                            binary_features=number_binary_columns)
 
     # run optimization
     for objective_func in objective_functions:
@@ -29,10 +28,16 @@ def main():
                 objective_function=objective_func,
                 save_results_dir=OPTIMIZATION_RESULTS_DIR,
                 auto_binary_boundaries=True,
-                swarm_size=200,
-                max_iterations=5000,
-                post_hoc_analysis=10)
+                swarm_size=500,
+                max_iterations=6000,
+                post_hoc_analysis=20)
+
+
+def make_plots():
+    plot_optimal_feature_distributions(results_dir=OPTIMIZATION_RESULTS_DIR,
+                                       save_dir=OPTIMIZATION_RESULTS_DIR)
 
 
 if __name__ == "__main__":
     main()
+    make_plots()
