c20ffeaed60ce04fa9f834537473ad75fbbdc2fd
Karl <karl_loza@sjtu.edu.cn>
2025-07-15 13:51:08 +0800

    Use feature range from constants

--- 6_optimization.py
+++ 6_optimization.py
@@ -1,15 +1,13 @@
 from ml_tools.PSO_optimization import run_pso, multiple_objective_functions_from_dir, plot_optimal_feature_distributions
 from ml_tools.utilities import deserialize_object
-from paths import OPTIMIZATION_RESULTS_DIR, OPTIMIZATION_MODELS_DIR, SERIALIZED_CONTINUOUS_FILE, SERIALIZED_BINARY_FILE, OPTIMIZATION_PLOTS_DIR
+from paths import OPTIMIZATION_RESULTS_DIR, OPTIMIZATION_MODELS_DIR, SERIALIZED_BINARY_FILE, OPTIMIZATION_PLOTS_DIR
+from helpers.constants import CONT_FEATURES_VALUE_RANGE
 
 
 def main():
-    # load value range for continuous features 
-    cont_features_value_range: dict[str, tuple[float,float]] = deserialize_object(SERIALIZED_CONTINUOUS_FILE) # type: ignore
-
     # set boundaries
-    lower_boundaries = [value[0] for key, value in cont_features_value_range.items()]
-    upper_boundaries = [value[1] for key, value in cont_features_value_range.items()]
+    lower_boundaries = [value[0] for key, value in CONT_FEATURES_VALUE_RANGE.items()]
+    upper_boundaries = [value[1] for key, value in CONT_FEATURES_VALUE_RANGE.items()]
 
     # Set number of binary columns
     binary_columns: list[str] = deserialize_object(SERIALIZED_BINARY_FILE) # type: ignore
--- helpers/constants.py
+++ helpers/constants.py
@@ -26,3 +26,22 @@ TARGETS = [
     "capacity_retention(%)", 
     "first_coulombic_efficiency(%)"
 ]
+
+# Range values set by the experts
+CONT_FEATURES_VALUE_RANGE = {
+    "particle_size_primary(um)": (0.0, 40.0),
+    "particle_size_secondary(um)": (0.0, 40.0),
+    "annealing_temperature_1(K)": (600.0, 1250.0),
+    "annealing_temperature_2(K)": (600.0, 1250.0),
+    "annealing_time_1(hour)": (0.0, 24.0),
+    "annealing_time_2(hour)": (0.0, 24.0),
+    "average_voltage(V)": (0.0, 5.0),
+    "electrolyte_molarity": (0.0, 10.0),
+    "cycles": (5, 3500)
+}
+
+TARGETS_VALUE_RANGE = {
+    TARGETS[0]: (0.0, 350.0),
+    TARGETS[1]: (0.0, 100.0),
+    TARGETS[2]: (0.0, 100.0)
+}
