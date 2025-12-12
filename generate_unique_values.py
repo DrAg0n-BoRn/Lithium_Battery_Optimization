be629e18a7e62baeedb147304b616709a4374c10
Karl <luigiloza@gmail.com>
2025-12-12 22:29:42 +0800

    update syntax

--- generate_unique_values.py
+++ generate_unique_values.py
@@ -2,6 +2,6 @@ from ml_tools.ETL_cleaning import save_unique_values
 from paths import PM
 
 if __name__ == "__main__":
-    save_unique_values(csv_path=PM.clean_data_file, 
+    save_unique_values(csv_path_or_df=PM.clean_data_file, 
                        output_dir=PM.clean_data, 
                        keep_column_order=True)
