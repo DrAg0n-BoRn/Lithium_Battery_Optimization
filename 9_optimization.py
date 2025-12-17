98d92a5ebd222a202a3faa4e92fe71d083dd6ca6
Karl <luigiloza@gmail.com>
2025-12-17 16:48:55 +0800

    Update optimization workflow and toolbox version
    Added creation of continuous bounds template in the engineering notebook and updated the optimization config to use bounds from the results directory. Upgraded dragon-ml-toolbox to version 19.10.0

--- 9_optimization.py
+++ 9_optimization.py
@@ -4,13 +4,13 @@ from ml_tools.ML_models_advanced import DragonNodeModel
 from ml_tools.ML_configuration import DragonParetoConfig
 from ml_tools.ML_utilities import ArtifactFinder
 
-from helpers.constants import CONTINUOUS_INTEGER_FEATURES, TARGET_capacity, TARGET_capacity_retention, TARGET_first_coulombic_eff, CONTINUOUS_OPTIMIZATION_RANGE
+from helpers.constants import CONTINUOUS_INTEGER_FEATURES, TARGET_capacity, TARGET_capacity_retention, TARGET_first_coulombic_eff
 from paths import PM
 
 
 def optimization_config():
     # Define optimization objectives
-    _objectives = {
+    objectives = {
         TARGET_capacity: 'max',
         TARGET_capacity_retention: 'max',
         TARGET_first_coulombic_eff: 'max'
@@ -19,9 +19,8 @@ def optimization_config():
     # Optimizer configuration
     PARETO_CONFIG = DragonParetoConfig(
         save_directory=PM.optimization_results,
-        target_objectives=_objectives, # type: ignore
-        # continuous_bounds_map=PM.optimization_results,
-        continuous_bounds_map=CONTINUOUS_OPTIMIZATION_RANGE,
+        target_objectives=objectives, # type: ignore
+        continuous_bounds_map=PM.optimization_results,
         columns_to_round=CONTINUOUS_INTEGER_FEATURES,
         population_size=500,
         generations=1000,
